e6224a51ccf6c7d0466b7142299a2d79468c8036
kamiru <kami16ru@gmail.com>
2020-04-29 01:16:13 +0300

    error middlewares

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const cors = require('cores');
+const cors = require('cors');
 
 const app = express();
 app.use(morgan('common'));
@@ -9,6 +9,28 @@ app.use(helmet());
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+
+app.get('/', (req, res) => {
+    res.json({
+       message: 'Hello world!'
+    });
+})
+
+app.use((req, res, next) => {
+    const error = new Error(`Not Found = ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+})
+
+// eslint-disable-no-unused-vars
+app.use((error, req, res, next) => {
+   const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+   res.status(statusCode);
+   res.json({
+      message: error.message,
+      stack: process.env.NODE_ENV === 'production' ? 'Привет бро!' : error.stack
+   });
+});
 
 const port = process.env.PORT || 1337
 app.listen(port, () => {
